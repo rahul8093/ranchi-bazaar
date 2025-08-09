869049cd5bdb88cac86ab1e94a37dd5fe699adf3
ele.rahul <rahul@elescript.com>
2025-08-09 19:43:10 +0530

    new update product

---
+++ app/product/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Product = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Shopping Product</h1>
+      <p>Your Product is currently empty.</p>
+    </main>
+  );
+};
+
+export default Product;
