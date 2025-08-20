4f25c5e5d8773728b1506b9bfbc742af6833f682
rahul8093 <100830613+rahul8093@users.noreply.github.com>
2025-08-21 01:40:14 +0530

    Dev (#10)
    * update auth api
    
    * navigation menu dynamic updated
    
    * navbar slug updated
    
    * update slug page
    
    * update homepage
    
    * hero-banner-update
    
    * fix build error
    
    * update ui
    
    * corusel ui change
    
    * update homepage banner
    
    * update logo
    
    * product card ui update
    
    * ui fix product card
    
    * pdp page created
    
    * link all product tp pdp
    
    * fix build errors
    
    * minor ui fix login signup pdp
    
    * bug-styling issue in pdp
    
    * cart page created
    
    * cart page updated and loading file is added
    
    ---------
    
    Co-authored-by: ele.rahul <rahul@elescript.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
-// app/page.tsx
-
-import HomePage from './homepage/page'; // Import the HomePage component
-// import { fetchProducts } from './lib/saleor/queries/fetchProducts';
+import HomePage from './homepage/page';
 import './styles/global.css'
 
-// Server component: Fetch data and pass it to the client component
 const Page = async () => {
-  // Fetch products server-side
-  // const products = await fetchProducts();
 
-  // Pass the products data to the client-side component
   return (
     <div>
-      {/* <Header/> */}
-  <HomePage />
-  {/* <Footer/> */}
+      <HomePage />
     </div>
-    
-);
+
+  );
 };
 
 export default Page;
