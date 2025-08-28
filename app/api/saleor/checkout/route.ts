d540a5925f8a4ab694d3e143d441b421ffddf87f
ele.rahul <rahul@elescript.com>
2025-08-28 14:49:15 +0530

    build issue fix

--- app/api/saleor/checkout/route.ts
+++ app/api/saleor/checkout/route.ts
@@ -1,4 +1,4 @@
-import { createCheckout } from '@/app/lib/saleor/helpers/checkout';
+// import { createCheckout } from '@/app/lib/saleor/helpers/checkout';
 import { NextRequest, NextResponse } from 'next/server';
 // import { createCheckout } from '@/lib/saleor/helpers/checkout';
 
@@ -12,9 +12,9 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'Missing email or cart items' }, { status: 400 });
     }
 
-    const checkoutData = await createCheckout(email, lines);
+    // const checkoutData = await createCheckout(email, lines);
 
-    return NextResponse.json(checkoutData);
+    // return NextResponse.json(checkoutData);
   } catch (error) {
     console.error('Error in /api/saleor/checkout:', error);
     return NextResponse.json({ error: 'Failed to create checkout' }, { status: 500 });
